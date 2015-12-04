f0e031577297f723a24be9b24b6df0d547b6c4fc
Pulkit Gupta <pulgupta@gmail.com>
2015-12-05 03:29:09 +0530

    updated

--- src/org/study/collections/GenericTips.java
+++ src/org/study/collections/GenericTips.java
@@ -8,13 +8,32 @@ import java.util.Set;
  * @description This class will show some neat tricks which will apply on collections
  * 
  */
-public class GenericTips {
+public class GenericTips<E extends Number> {
+	
+	private E[] elements;
+	Set<E> ssss;
+	public GenericTips() {
+		//will not work
+		//elements = new E[90];
+		//To make this work we will create an object array and then will cast it into E array
+		//Also one point to take from here we can just not type cast primitive elements or objects
+		//We can even typecast a complete array
+		elements = (E[])new Object[10];
+		ssss = new HashSet<>();
+	}
+	
+	//In this way we can make a method generic without actually changing the class
+	public static <K> Set<K> testType() {
+		return new HashSet<K>();
+	}
+	
 	public static void main(String args[]) {
 		//A set which can contain object of some unknown type
 		
 		Set<? extends Number> s = new HashSet<>();
 		Set<?> ss = new HashSet<>();
 		Set<Object> sss = new HashSet<>();
+		 
 
 		//not working
 		//s.add(new Integer(10));
