1969c197357b3f6e05745fe193515dc9ee23913d
Pulkit Gupta <pulgupta@gmail.com>
2016-04-17 10:33:00 +0530

    more code

--- src/org/study/corejava/BigDecimalTest.java
+++ src/org/study/corejava/BigDecimalTest.java
@@ -6,11 +6,17 @@ public class BigDecimalTest {
 
 	public static void main(String args[]) {
 		
-		BigDecimal bd = new BigDecimal("1.50");
+		BigDecimal bd = new BigDecimal("1.55");
 		//you will see that this will result in a very large approximate value.
-		BigDecimal bd1 = new BigDecimal(.11111111111111);
+		BigDecimal bd1 = new BigDecimal(1.55);
+		//For this reason we should always use the string constructor and not the double one
 		System.out.println(bd);
 		System.out.println(bd1);
+		
+		//We have a way in which we can restrict the number of trailing 
+		//random value which is being generated in case of BigDecimal with double values.
+		bd1=bd1.setScale(4, BigDecimal.ROUND_HALF_EVEN);
+		System.out.println(bd1.stripTrailingZeros().toPlainString());
 		
 	}
 }
