e02e60f0d2f76b035234422ccfc9f27183624e03
Pulkit Gupta <pulgupta@gmail.com>
2016-04-17 19:15:05 +0530

    interface facts

--- src/org/study/corejava/InterfaceTest.java
+++ src/org/study/corejava/InterfaceTest.java
@@ -9,4 +9,43 @@ public interface InterfaceTest {
 	//static - not allowed below
 	void print();
 	
+	static void printThis() {
+		System.out.println("We can have method with bodies in Interfaces");
+	}
+	
+	/**
+	 * Its a new feature in Java 8 where we can add methods to interfaces
+	 */
+	default void oneMore() {
+		System.out.println("A new default method");
+	}
+	
+}
+
+class testerInterface implements InterfaceTest{
+	
+	public static void main(String[] args) {
+		testerInterface ti = new testerInterface();
+		ti.print();
+		ti.oneMore();
+		/**
+		 * Because this is a static method we were not able to call it using the ti object
+		 */
+		InterfaceTest.printThis();
+		/**
+		 * We can however call a class static method using an object.
+		 * However it will throw a warning but nothing else
+		 */
+		ti.printOneMore();
+	}
+
+	@Override
+	public void print() {
+		System.out.println("This is the only method which is overridden");
+		
+	}
+	
+	public static void printOneMore() {
+		System.out.println("A class static method");
+	}
 }
