b7f946b2e5c6316d4e1ef9fc0f6ebb72d7210c3a
Pulkit Gupta <pulgupta@gmail.com>
2016-04-21 02:17:36 +0530

    threading examples

--- src/org/study/threading/ThreadnException.java
+++ src/org/study/threading/ThreadnException.java
@@ -2,20 +2,25 @@ package org.study.threading;
 
 class ExceptionGenerator implements Runnable{
 	
-	/**
-	 * Thus from this example we can see that there is now way of getting 
-	 * a checked exception from the run method.
-	 */
 	
+	/**
+	 * This method will throw runtime exception 
+	 * When we will call this method 
+	 * @return
+	 */
 	private int divide() {
 		int result = 10/0;
 		return result;
 	}
-	
+	/**
+	 * Thus from this example we can see that there is now way of getting 
+	 * a checked exception from the run method.
+	 */
 	@Override
 	public void run () {
 		System.out.println("This is inside run");
 		this.divide();
+		System.out.println("This is after divide");    //This will not be printed
 		//throw new InitializationError("Ohh this is an error");
 	}
 	
