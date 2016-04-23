3b8ccc72a4402ecf10bf8392bb2f9bbb4c549709
Pulkit Gupta <pulgupta@gmail.com>
2016-04-23 21:32:29 +0530

    regex tests

--- src/org/study/regular/expressions/AllPossibleRegularExpression.java
+++ src/org/study/regular/expressions/AllPossibleRegularExpression.java
@@ -3,15 +3,32 @@ package org.study.regular.expressions;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * This class will contains all the tricks and tips for regular expression
+ * As a good point to remember if you see we can use string and even string builder 
+ * for giving the string to test
+ * @author pulgupta
+ *
+ */
 public class AllPossibleRegularExpression {
 
 	public static void main(String[] args) {
 		String pattern1 = "[A-Z]{1}[a-z]*";
 		String data1 = "Pulkit";
+		StringBuilder data2 = new StringBuilder("Gargi");
 		AllPossibleRegularExpression.tryPattern(pattern1, data1);
+		AllPossibleRegularExpression.tryPattern(pattern1, data2);
 	}
 	
 	public static void tryPattern(String inputPattern, String data) {
+		Pattern pattern = Pattern.compile(inputPattern);
+		Matcher matcher = pattern.matcher(data);
+		if(matcher.matches())
+			System.out.println("****What a great thing dude****");
+		else
+			System.out.println("****Try something else****");
+	}
+	public static void tryPattern(String inputPattern, StringBuilder data) {
 		Pattern pattern = Pattern.compile(inputPattern);
 		Matcher matcher = pattern.matcher(data);
 		if(matcher.matches())
