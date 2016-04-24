e4ad12f752a8a0a106cbff05ffe4c50649784c26
Pulkit Gupta <pulgupta@gmail.com>
2016-04-24 16:56:54 +0530

    BST operations

--- src/org/study/algos/SortedArrayToBST.java
+++ src/org/study/algos/SortedArrayToBST.java
@@ -13,9 +13,10 @@ public class SortedArrayToBST {
 		SortedArrayToBST satb = new SortedArrayToBST();
 		BstNode root = satb.convert(array, 0, array.length-1);
 		satb.inorderTraversal(root);
+		System.out.println(satb.getDepth(root));
 		
 	}
-	
+
 	public BstNode convert(int[] array, int low, int high) {
 		if(low<=high) {
 			int middle = (low+high)/2;
@@ -37,5 +38,25 @@ public class SortedArrayToBST {
 		if(root.right!=null) {
 			inorderTraversal(root.right);
 		}	
+	}
+	
+	/**
+	 * Get the maximum depth of a binary search tree
+	 * @param root : the root of the binary search tree
+	 * @return : depth
+	 */
+	private int getDepth(BstNode root) {
+		if(root==null)
+			return 0;
+		else {
+			int left = getDepth(root.left);
+			int right = getDepth(root.right);
+			
+			if(left>right)
+				return left+1;
+			else
+				return right+1;
+		}
+		
 	}
 }
