b8d1273e51958bd89fdb43e329fc37f1f2c9ff5a
Pulkit Gupta <pulgupta@gmail.com>
2016-05-11 14:13:16 +0530

    added more

--- src/org/study/inheritance/Super.java
+++ src/org/study/inheritance/Super.java
@@ -8,17 +8,28 @@ class SuperTest {
 	public SuperTest() {
 		System.out.println("This is in the parent class");
 	}
-	
+	public SuperTest(String a){
+		System.out.println("!!HAHA!! " + a);
+	}
 	public void someDummyMethod(){
 		System.out.println("Some dummy method");
 	}
 }
 public class Super extends SuperTest{
 	public Super() {
+		super("Jhon");
 		System.out.println("This is in the sub class");
 		//super(); - This is an error as we cannot call super as
 		//it should be the first statement in the sub class constructor
 		super.someDummyMethod();
+	}
+	
+	
+	public void someDummyMethod(String overloaded){
+		System.out.println("Some dummy method");
+	}
+	public void someDummyMethod(String overloaded, int again){
+		System.out.println("Some dummy method");
 	}
 	
 	public  void someDummySecondMethod() {
