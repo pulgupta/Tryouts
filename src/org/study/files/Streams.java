18b5de8c43c2cce364cf1eb326b8c40a37b26c3e
Pulkit Gupta <pulgupta@gmail.com>
2016-05-11 18:16:10 +0530

    added more

---
+++ src/org/study/files/Streams.java
@@ -0,0 +1,27 @@
+package org.study.files;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Streams {
+
+	public static void main(String[] args) throws IOException {
+		InputStream is = new FileInputStream("/Users/pulgupta/Documents/JavaFileTest/user1.txt");
+		//based on the size of the buffer it will read the data
+		final int BUFFER_SIZE = 10;
+		byte[] b=new byte[BUFFER_SIZE];
+		//Lets read the whole file based on the buffers
+		//In this case we are reading the file till we have enough data
+		//and at the end we will read the remaining buffer.
+		System.out.println(is.available());
+		while(is.available()>BUFFER_SIZE){
+			is.read(b);
+			System.out.print(new String(b));
+		}
+		byte[] b1 = new byte[is.available()];
+		is.read(b1);
+		System.out.println(new String(b1));
+		is.close();
+	}
+}
