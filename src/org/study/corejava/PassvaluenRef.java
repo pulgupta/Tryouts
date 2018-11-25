c3e7e87a409738e91f4d03edb587ac16e1845cae
Pulkit Gupta <pulgupta@gmail.com>
2018-11-25 18:16:49 +0530

    added more examples

--- src/org/study/corejava/PassvaluenRef.java
+++ src/org/study/corejava/PassvaluenRef.java
@@ -3,12 +3,35 @@ package org.study.corejava;
 import org.study.genericClass.Employee;
 
 //This test will prove that primitive types are passed by values 
-//and all the objects are passed by reference
+//and all the objects are passed by value of their reference
 //Thus primitive values changed inside a function will not affect the passed variable
 //However if we update some values of an object inside the function it will result
 //in the mutation of the original object itself.
 public class PassvaluenRef {
-	
+	public static void main(String args[]) {
+		int d = 9;
+		String e = "String";
+		Employee f = new Employee();
+		f.setAge(15);
+		System.out.println("values are " + d + " " + e + " " + f.getAge());
+		TesterRef.update(d, e, f);
+		System.out.println("values after update are " + d + " " + e + " " + f.getAge());
+		
+		TesterRef x1 = new TesterRef();
+		TesterRef x2 = new TesterRef();
+		TesterRef x3 = new TesterRef();
+		x1.a =10;
+		x3.a =20;
+		x2 = x1;
+		x2.a = 30;
+		System.out.println(x1.a + " " + x2.a + " " +x3.a);
+	}
+
+}
+
+
+//Test class
+class TesterRef {
 	static volatile Integer y = 0;
 	public int a;
 	public static void update(int a, String b, Employee c) {
@@ -21,30 +44,6 @@ public class PassvaluenRef {
 		finally {
 			y = new Integer(20);
 		}
-		System.out.println(y);
-	}
-
-}
-
-
-//Test class
-class TesterRef {
-	public static void main(String args[]) {
-		int d = 9;
-		String e = "String";
-		Employee f = new Employee();
-		f.setAge(15);
-		System.out.println("values are " + d + " " + e + " " + f.getAge());
-		PassvaluenRef.update(d, e, f);
-		System.out.println("values are update are" + d + " " + e + " " + f.getAge());
-		
-		PassvaluenRef x1 = new PassvaluenRef();
-		PassvaluenRef x2 = new PassvaluenRef();
-		PassvaluenRef x3 = new PassvaluenRef();
-		x1.a =10;
-		x3.a =20;
-		x2 = x1;
-		x2.a = 30;
-		System.out.println(x1.a + " " + x2.a + " " +x3.a);
+		System.out.println("update has completed execution");
 	}
 }
