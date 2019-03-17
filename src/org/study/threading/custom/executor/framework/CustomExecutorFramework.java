4a36f449c2fdeeb31608b1915171499900f1f7c2
Pulkit Gupta <pulgupta@gmail.com>
2019-03-17 20:32:25 +0530

    coming up with a custom thread pool in java

--- src/org/study/threading/custom/executor/framework/CustomExecutorFramework.java
+++ src/org/study/threading/custom/executor/framework/CustomExecutorFramework.java
@@ -4,22 +4,46 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
- * We will be creating a custome executor framework with fixed pool of threads
- * The idea is a pool of thread will be maintained and once a thread is created it will
- * keep on executing tasks
+ * We will be creating a custom executor framework with fixed pool of threads
+ * The idea is a pool of thread will be maintained and once a thread is created
+ * it will keep on executing tasks
+ * 
  * @author pulgupta
  */
 public class CustomExecutorFramework {
 
-	int poolSize = 0;
-	BlockingQueue<Runnable> taskQueue = null;
+	private BlockingQueue<Runnable> taskQueue = null;
+
 	public CustomExecutorFramework(int poolSize) {
-		this.poolSize = poolSize; 
 		// The queue can accept any number of tasks and so is left open ended
-		taskQueue = new LinkedBlockingQueue<>(); 
+		taskQueue = new LinkedBlockingQueue<>();
+		for (int i = 0; i < poolSize; i++) {
+			Thread t = new Thread(new TaskExecutor(poolSize, taskQueue), "poolThread" + (i + 1));
+			t.start();
+		}
 	}
-	
+
 	public void submitTask(Runnable r) {
 		taskQueue.add(r);
+	}
+
+	private static class TaskExecutor implements Runnable {
+
+		BlockingQueue<Runnable> taskQueue;
+
+		public TaskExecutor(int size, BlockingQueue<Runnable> queue) {
+			this.taskQueue = queue;
+		}
+
+		@Override
+		public void run() {
+			while (true) {
+				Runnable r = taskQueue.poll();
+				if (r != null) {
+					System.out.print("\n" + Thread.currentThread().getName() + "\n");
+					r.run();
+				}
+			}
+		}
 	}
 }
--- src/org/study/threading/custom/executor/framework/Tester.java
+++ src/org/study/threading/custom/executor/framework/Tester.java
@@ -2,4 +2,37 @@ package org.study.threading.custom.executor.framework;
 
 public class Tester {
 
+	public static void main(String[] args) {
+		CustomExecutorFramework cef = new CustomExecutorFramework(4);
+		cef.submitTask(() -> {
+			for(int i=0; i<10; i++) {
+				System.out.print("1-" + i + " ");
+			}
+		});
+		cef.submitTask(() -> {
+			for(int i=0; i<10; i++) {
+				System.out.print("2-" + i + " ");
+			}
+		});
+		cef.submitTask(() -> {
+			for(int i=0; i<10; i++) {
+				System.out.print("3-" + i + " ");
+			}
+		});
+		cef.submitTask(() -> {
+			for(int i=0; i<10; i++) {
+				System.out.print("4-" + i + " ");
+			}
+		});
+		cef.submitTask(() -> {
+			for(int i=0; i<10; i++) {
+				System.out.print("5-" + i + " ");
+			}
+		});
+		cef.submitTask(() -> {
+			for(int i=0; i<10; i++) {
+				System.out.print("6-" + i + " ");
+			}
+		});
+	}
 }
