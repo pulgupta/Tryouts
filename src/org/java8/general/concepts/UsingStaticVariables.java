ed12fc0386f9502f9f8376ffad27e1888ef9be2a
Pulkit Gupta <pulgupta@gmail.com>
2019-04-26 13:40:38 +0530

    added variable access in lambdas

--- src/org/java8/general/concepts/UsingStaticVariables.java
+++ src/org/java8/general/concepts/UsingStaticVariables.java
@@ -4,13 +4,28 @@ import java.util.function.Supplier;
 
 public class UsingStaticVariables {
 
-	static int staticTest = 10;
+	static int staticTest = 5;
+	int var = 10;
+	
+	public void testInstanceVar()
+	{
+		var = 10;
+		// We can use any instance variable or static variable in the lambdas
+		// However we can't use a local variable if it is not marked as 
+		// Final or if it is not effectively final
+		int data = 10;
+		data++;
+		Supplier<Integer> supplier = () -> {return staticTest + var; };
+		System.out.println(supplier.get());
+	}
 	
 	public static void main(String args[]) {
+		UsingStaticVariables obj = new UsingStaticVariables();
+		obj.testInstanceVar();
 		
 		staticTest++;
 		System.out.println("Static test first value is " + staticTest);
-		Supplier<Integer> supplier = () -> {return staticTest++; };
+		Supplier<Integer> supplier = () -> {return ++staticTest; };
 		staticTest++;
 		System.out.println("Static test second value is " + staticTest);
 		System.out.println("Static Test lambda output is " + supplier.get());
