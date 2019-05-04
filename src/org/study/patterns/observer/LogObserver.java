f9833ddbf758f3793a75c22e35541ea953b0d40b
Pulkit Gupta <pulgupta@gmail.com>
2019-05-04 07:30:50 +0530

    added state design pattern

--- src/org/study/patterns/observer/LogObserver.java
+++ src/org/study/patterns/observer/LogObserver.java
@@ -1,5 +1,10 @@
 package org.study.patterns.observer;
-
+/**
+ * Observer instance knows for which subject instance it has to observe
+ * This helps when the observer wants to get the state of the subject on which it is 
+ * being called.
+ * @author pulgupta
+ */
 public class LogObserver extends Observer{
 
 	public LogObserver(Subject subject) {
