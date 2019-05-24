9d77515c234b8e3431f4bac26ce60ed72b5a5096
Pulkit Gupta <pulgupta@gmail.com>
2019-05-24 17:38:17 +0530

    some more hands on

--- src/org/study/corejava/StringTests.java
+++ src/org/study/corejava/StringTests.java
@@ -1,11 +1,22 @@
 package org.study.corejava;
 
+import java.util.Arrays;
+
 public class StringTests {
 	public static void main(String[] args) throws InterruptedException {
-		for(int i=0;i<1_000_000_000;i++) {
-			String a = "Hello " + i; 
-		}
-		System.out.println("String allocated");
-		Thread.sleep(10000000);
+		String name = "pulkit,gargi";
+		String[] names= name.split(",");
+		Arrays.stream(names).forEach(System.out::println);
+		
+		String email = "@.@.";
+		String email1= email.substring(email.lastIndexOf("@")+1);
+		System.out.println(email1);
+		System.out.println(email1.substring(0, email1.lastIndexOf(".")));
+		
+//		for(int i=0;i<1_000_000_000;i++) {
+//			String a = "Hello " + i; 
+//		}
+//		System.out.println("String allocated");
+//		Thread.sleep(10000000);
 	}
 }
